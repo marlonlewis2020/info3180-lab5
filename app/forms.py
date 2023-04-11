d4180f296a9a7bbe0fb39fdf7575c4b24ffa499c
marlonlewis2020 <marlonlewis483@gmail.com>
2023-04-11 17:45:51 -0500

    updated textArea to required

--- app/forms.py
+++ app/forms.py
@@ -2,9 +2,9 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired, FileAllowed, FileSize
 from wtforms import StringField, TextAreaField
-from wtforms.validators import InputRequired
+from wtforms.validators import InputRequired, DataRequired
 
 class MovieForm (FlaskForm):
     title = StringField('Title', validators=[InputRequired()])
-    description = TextAreaField('Description', validators=[InputRequired()])
+    description = TextAreaField('Description', validators=[DataRequired()])
     poster = FileField('Poster', validators=[FileRequired(), FileSize(max_size=4000000), FileAllowed(["jpg", "png"], message="Image Files Only!")])
